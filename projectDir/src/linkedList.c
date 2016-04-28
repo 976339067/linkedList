2257666434c7f139a318c3efe86f3abb7c355402
zhuxh <976339067@qq.com>
2016-04-28 22:28:14 +0800

    log:增加了链表测试功能,并用宏__LIST_TEST__控制.

--- projectDir/src/linkedList.c
+++ projectDir/src/linkedList.c
@@ -4,5 +4,15 @@
  *  Created on: Apr 28, 2016
  *      Author: hui
  */
+#include <stdio.h>
 
+#define	__LIST_TEST__
 
+#ifdef __LIST_TEST__
+int main(int argc, char *argv[])
+{
+	printf("in %s, line :%d\n", __func__, __LINE__);
+
+	return 0;
+}
+#endif
