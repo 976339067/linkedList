2658969e73d1ace9fd655aa2e2b526b46883d725
zhuxh <976339067@qq.com>
2016-04-28 22:06:44 +0800

    log:创建链表节点的数据结构listNode_t.

--- projectDir/h/linkedList.h
+++ projectDir/h/linkedList.h
@@ -8,6 +8,13 @@
 #ifndef PROJECTDIR_H_LINKEDLIST_H_
 #define PROJECTDIR_H_LINKEDLIST_H_
 
+/*
+ * list node type
+ */
+typedef struct listNode{
+	struct listNode *previous;
+	struct listNode *next;
+}listNode_t;
 
 
 #endif /* PROJECTDIR_H_LINKEDLIST_H_ */
