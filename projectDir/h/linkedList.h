18961db25a76be2cfb3ca56007e2aa371246f87e
zhuxh <976339067@qq.com>
2016-04-28 22:22:20 +0800

    log:定义链表头的数据结构listHead_t,用于保存链表的长度,头节点和尾节点.

--- projectDir/h/linkedList.h
+++ projectDir/h/linkedList.h
@@ -12,9 +12,16 @@
  * list node type
  */
 typedef struct listNode{
-	struct listNode *previous;
-	struct listNode *next;
+	struct listNode *previous;	/*previous node pointer*/
+	struct listNode *next;		/*next node pointer*/
 }listNode_t;
 
+/*
+ * list head type
+ */
+typedef struct listHead{
+	int len;			/*list length*/
+	listNode_t node;	/*list head node pointer and list tail node pointer*/
+};
 
 #endif /* PROJECTDIR_H_LINKEDLIST_H_ */
